32f9959c6ee6b51ca89217be878eadbe19a66017
remorses <beats.by.morse@gmail.com>
2020-09-06 16:25:57 +0200

    init

---
+++ example/tests/1.test.ts
@@ -0,0 +1,44 @@
+import { createClient } from "../generated";
+
+const client = createClient();
+
+test("first query", async () => {
+  const res = await client.query({
+    continents: {
+      code: 1,
+      name: 1
+    }
+  });
+  expect(res.continents).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "code": "AF",
+        "name": "Africa",
+      },
+      Object {
+        "code": "AN",
+        "name": "Antarctica",
+      },
+      Object {
+        "code": "AS",
+        "name": "Asia",
+      },
+      Object {
+        "code": "EU",
+        "name": "Europe",
+      },
+      Object {
+        "code": "NA",
+        "name": "North America",
+      },
+      Object {
+        "code": "OC",
+        "name": "Oceania",
+      },
+      Object {
+        "code": "SA",
+        "name": "South America",
+      },
+    ]
+  `);
+});
